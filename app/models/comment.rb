a30fbf0ebb64f6f146f473012c2b72b2e4ac0c5e
Ntare Guy <gntare2@gmail.com>
2022-10-22 15:29:45 +0200

    Fix linter issues

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,13 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: :author_id
-    belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post
 
-    # A method that updates the comments counter for a post.
-    after_save :update_comments_counter
+  # A method that updates the comments counter for a post.
+  after_save :update_comments_counter
 
-    def update_comments_counter
-      post.increment!(:comments_counter)
-    end
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 
-    private :update_comments_counter
+  private :update_comments_counter
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,12 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: :author_id
-    belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post
 
-    after_save :update_likes_counter
-    # Method that updates the likes counter for a post.
-    def update_likes_counter
-      post.increment!(:likes_counter)
-    end
+  after_save :update_likes_counter
+  # Method that updates the likes counter for a post.
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
 
-    private :update_likes_counter
+  private :update_likes_counter
 end
