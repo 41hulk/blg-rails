c88ca31f68acc4d03bd0ed11b87d8c8199dc5024
Ntare Guy <gntare2@gmail.com>
2022-10-29 00:14:36 +0200

    Posts Spec

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  context 'Correct response status' do
+    describe 'GET /index' do
+      it 'returns http success' do
+        get '/users/:user_id/posts'
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'GET /show' do
+      it 'returns http success' do
+        get '/users/:user_id/posts/:id'
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+  
+  context 'Correct template' do
+    describe 'GET posts' do
+      it 'renders the index template' do
+        get '/users/:user_id/posts'
+        expect(response).to render_template(:index)
+        expect(response.body).to include('Here is a list of all posts for a given user')
+      end
+      it 'renders the show template' do
+        get '/users/:user_id/posts/:id'
+        expect(response).to render_template(:show)
+        expect(response.body).to include('Here is a post by a user given an id')
+      end
+    end
   end
 end
