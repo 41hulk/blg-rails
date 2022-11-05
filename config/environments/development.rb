38d74ee9bbc891e59ab8058896c9ab2f7e8a1f8a
Ntare Guy <gntare2@gmail.com>
2022-11-05 23:46:24 +0200

    redirect

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,9 @@ Rails.application.configure do
 
   # smtp
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
   # Raise exceptions for disallowed deprecations.
